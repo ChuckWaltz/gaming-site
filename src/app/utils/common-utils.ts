22b9158b7271933753509f051f3f413d4aba9357
Chuck Waltz <chuckwaltz@gmail.com>
2020-06-07 11:20:53 -0400

    Styling updates for header and story-grid components

--- src/app/utils/common-utils.ts
+++ src/app/utils/common-utils.ts
@@ -2,7 +2,7 @@ import { ScreenSize } from '../enums/screen-size.enum';
 
 export const getScreenSize = (): ScreenSize => {
   let width = window.innerWidth;
-  if (width >= 1100) return ScreenSize.XL;
+  if (width >= 1200) return ScreenSize.XL;
   if (width >= 728) return ScreenSize.Large;
   if (width >= 600) return ScreenSize.Medium;
   return ScreenSize.Small;
