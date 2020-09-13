7a01501ceace8bfae907a2feea1e34662d5391a6
ChuckWaltz <chuckwaltz@gmail.com>
2020-09-13 14:57:04 -0400

    Added some custom rendering options for contentful rich text

--- src/app/components/pages/news-page/news-page.component.ts
+++ src/app/components/pages/news-page/news-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { StoryService } from 'src/app/services/story-service/story.service';
 import { Entry } from 'contentful';
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { MARKS, BLOCKS, INLINES } from '@contentful/rich-text-types';
 
 @Component({
   selector: 'app-news-page',
@@ -14,6 +15,28 @@ export class NewsPageComponent implements OnInit {
     private route: ActivatedRoute,
     private storyService: StoryService
   ) {
+    const richTextOptions = {
+      renderMark: {
+        [MARKS.CODE]: (text) => `<code>${text}</code>`,
+      },
+      renderNode: {
+        [BLOCKS.EMBEDDED_ASSET]: (node, next) =>
+          `<img src='${node.data.target.fields.file.url}'/>`,
+        [BLOCKS.EMBEDDED_ENTRY]: (node, next) =>
+          `<a class='embeddedEntry' href='${window.location.origin}/news/${node.data.target.sys.id}'>
+            <img src='${node.data.target.fields.mainImage.fields.file.url}'/>
+            <div class='embeddedEntryContent'>
+              <div class='embeddedEntryTitle'>${node.data.target.fields.title}</div>
+              <div class='embeddedEntrySubText'>${node.data.target.fields.subText}</div>
+            </div>
+          </a>`,
+        [INLINES.EMBEDDED_ENTRY]: (node, next) =>
+          `<div class='embeddedEntryInline'>
+            <div class='embeddedEntryInlineTitle'>${node.data.target.fields.title}</div>
+          </div>`,
+      },
+    };
+
     route.params.subscribe((val) => {
       window.scrollTo(0, 0);
 
@@ -21,7 +44,10 @@ export class NewsPageComponent implements OnInit {
 
       this.storyService.getStory(storyId).then((story) => {
         this.story = story;
-        this.storyBodyContent = documentToHtmlString(story.fields.body);
+        this.storyBodyContent = documentToHtmlString(
+          story.fields.body,
+          richTextOptions
+        );
         console.log(story);
         console.log(this.storyBodyContent);
       });
